fc3d6b6c4e7ab787daeb4bd1795a3972b30b476d
pdor <pdor92@hotmail.it>
2022-03-10 18:15:07 +0100

    Create addBookCards function and call it.
    This function adds the book cards from the library to the DOM.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let myLibrary = [];
 
 function Book(title, author, year, read) {
@@ -13,3 +15,32 @@ function addBookToLibrary(book) {
 
 // placeholder book
 myLibrary.push(new Book("Placehoder Title", "Max Power", "1969", true));
+
+function addBookCard(Book) {
+    let container = document.querySelector(".book-container");
+
+    let card = document.createElement("div");
+    let title = document.createElement("p");
+    let author = document.createElement("p");
+    let read = document.createElement("p");
+
+    title.innerText = Book.title;
+    author.innerText = `${Book.author}, ${Book.year}`;
+    read.innerText = Book.read ? "Already read" : "Still unread";
+
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(read);
+
+    card.classList.add("book-card");
+    card.classList.add(Book.read ? "read" : "unread");
+    container.appendChild(card);
+}
+
+function addBookCards(library) {
+    for (const b of library) {
+        addBookCard(b);
+    }
+}
+
+addBookCards(myLibrary);
